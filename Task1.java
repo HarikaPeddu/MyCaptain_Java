bf13ca7a7890d6826c0694084860528ce7b25f7d
HarikaPeddu <56473385+HarikaPeddu@users.noreply.github.com>
2021-04-24 15:39:18 +0530

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,15 @@
+
+public class Task1 {
+	public void FirstProgram() {
+		System.out.println("Hello World");
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		Task1 t = new Task1();
+		t.FirstProgram();
+		
+	}
+
+}
