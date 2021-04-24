9be28a2e45dd3f8c96cf7fbbaa7242253112d71d
HarikaPeddu <56473385+HarikaPeddu@users.noreply.github.com>
2021-04-24 16:15:04 +0530

    Add files via upload

---
+++ Task7.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Task7 {
+	public static void main(String args[]){    
+		  int i,m=0,flag=0;    
+		  System.out.println("Enter the number:");
+		  Scanner sc = new Scanner(System.in);
+		  int n=sc.nextInt();
+		  m=n/2;      
+		  if(n==0||n==1){  
+		   System.out.println(n+" is not prime number");      
+		  }else{  
+		   for(i=2;i<=m;i++){      
+		    if(n%i==0){      
+		     System.out.println(n+" is not prime number");      
+		     flag=1;      
+		     break;      
+		    }      
+		   }      
+		   if(flag==0)  { System.out.println(n+" is a prime number"); }  
+		  } 
+		}   
+
+}
